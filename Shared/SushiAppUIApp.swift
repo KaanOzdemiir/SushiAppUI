7fa7b3212458f957adad65497a3b55ed06de6ca4
Kaan Ozdemir <kaan.ozdemir@tsoft.com.tr>
2020-12-19 19:27:48 +0300

    Initial Commit

---
+++ Shared/SushiAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SushiAppUIApp.swift
+//  Shared
+//
+//  Created by Kaan Ozdemir on 19.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SushiAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
